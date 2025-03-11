558f2e63c8c76434efd98946a94dffa133198a2b
aaboeh <aapo.aarvala@gmail.com>
2025-03-11 15:07:13 +0200

    another commit

--- app/src/main/java/com/example/viikko9/NoteStorage.java
+++ app/src/main/java/com/example/viikko9/NoteStorage.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class NoteStorage {
     private ArrayList<Note> notes;
     private static NoteStorage noteStorage = null;
+
+    private NoteStorage() {
+        this.notes = new ArrayList<>();
+    }
     public static NoteStorage getInstance() {
         if (noteStorage == null) {
             noteStorage = new NoteStorage();
